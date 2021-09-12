069e5fb32134d7b6fbf41b0d234bc36ba5653888
jaquielajoie <jaquielajoie@gmail.com>
2021-09-12 12:18:18 -0400

    django settings

--- miye/miye/settings.py
+++ miye/miye/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'db.apps.DbConfig',
+    'service_menu.apps.ServiceMenuConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
